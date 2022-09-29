80b6e423b38baff7fe13ecb5c4bc2e4dca649bef
lagon <lagon@DESKTOP-IVS9RIG>
2022-09-29 17:28:09 +0100

    Kata2 versión 4

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,21 +1,21 @@
 package kata2;
 
-import java.util.HashMap;
 import java.util.Map;
+import kata2.HistogramGenerator;
 
 public class Kata2 {
 
-     public static void main(String[] args) {
-        int [] data = {1, 6, 2, 8, 3, 8, 2, 7, 3, 4, 2, 9, 0, 6};
-        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+    public static void main(String[] args) {
+        Integer [] data = {1, 6, 2, 8, 3, 8, 2, 7, 3, 4, 2, 9, 0, 6};
         
-        for(int i=0; i < data.length; i++){
-            histogram.put(data[i], histogram.containsKey(data[i]) ? histogram.get(data[i]) + 1 : 1);
-        }
+        HistogramGenerator histogramGenerator = new HistogramGenerator(data);
+        Map<Integer, Integer> histogram = histogramGenerator.getHistogram();
         
         for(Map.Entry<Integer, Integer> entry : histogram.entrySet()){
             System.out.println(entry.getKey() + " --> " + entry.getValue());    
-        }   
+        }
+        
+        
     }
     
 }
